68e6ad1c1e0449fb0a660a9cd44036b5d336784a
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2024-09-10 10:47:38 -0500

    Inicializando el proyecto de React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const componente = <h1>Hola mundo</h1>;
   return (
     <div className="App">
       <header className="App-header">
